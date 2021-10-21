58fd01b10f70af7477adb807bd0aed92b5e68467
mokuo <tennis10988.yk@gmail.com>
2021-10-21 22:13:55 +0900

    tmp2

--- src/lib/__tests__/parseHtmlTable.test.ts
+++ src/lib/__tests__/parseHtmlTable.test.ts
@@ -1,11 +1,7 @@
 import { readFile } from "fs/promises";
 import { join } from "path";
 import { path as appRootPath } from "app-root-path";
-import parseHtmlTable, { TableCellsStock } from "../parseHtmlTable";
-
-describe("TableCellsStock", () => {
-  
-})
+import parseHtmlTable from "../parseHtmlTable";
 
 describe("parseHtmlTable()", () => {
   // TODO: src/lib/__tests__/__fixtures__/20211001_31_higashi_itabashi_3_4.html を読み込む
--- src/lib/parseHtmlTable.ts
+++ src/lib/parseHtmlTable.ts
@@ -1,46 +1,36 @@
 import { JSDOM } from "jsdom";
-
-type Cell = {
-  rowIndex: number
-  columnIndex: number
-  value: string
-}
-export class TableCellsStock {
-  private stock: Cell[]
-
-  constructor() {
-    this.stock = []
-  }
-
-  public findCell(rowIndex: number, columnIndex: number): string | undefined {
-    const cell = this.stock.find(_cell => _cell.rowIndex === rowIndex && _cell.columnIndex === columnIndex)
-    return cell && cell.value
-  }
-
-  public stockCell(rowIndex: number, columnIndex: number, value: string) {
-    
-  }
-}
+import KeyValueQueue from "./KeyValueQueue";
 
 const parseHtmlTable = (htmlTable: string): string[][] => {
   const table: string[][] = []
+  const queue = new KeyValueQueue()
 
   const {document} = new JSDOM(htmlTable).window
   const trNodeList = document.querySelectorAll("table[summary='選択した施設・時間帯の空き状況を確認するための表。'] tbody tr")
+
   // 各行の中で forEach で回して、スタックから取って詰めていく
   trNodeList.forEach((trElement, rowIndex) => {
     const row: string[] = []
-    const cellNodeList = trElement.querySelectorAll("th,td") // th か td
+    const cellNodeList = trElement.querySelectorAll("th,td") // cell = <th> or <td>
 
     const endOfRow = false
-    const columnIndex = 0
+    let columnIndex = 0
     while (endOfRow) {
-      // column のスタックがあれば使う
-      // j を渡して row のスタックがあれば使う
+      // キューにあれば使う
       // なければ cellNodeList を使う
         // rowspan, colspan があればスタックする
+      const poppedCell = queue.dequeue(`${rowIndex}:${columnIndex}`)
+      if (poppedCell) {
+        table[rowIndex][columnIndex] = poppedCell
+      } else {
+        const cellElement = cellNodeList[columnIndex]
+        table[rowIndex][columnIndex] = cellElement.textContent || ""
 
-      j += 1
+        // TODO: rowspan, colspan があればスタックする
+      }
+
+      columnIndex += 1
+      // TODO: endOfRow の判定処理
     }
 
     table.push(row)
